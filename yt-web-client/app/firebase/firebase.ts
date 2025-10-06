3868f3296894fa74f36251aa7ea0f3afeed14896
Amal Satheesan <133629513+kutt27@users.noreply.github.com>
2025-10-06 22:54:37 +0530

    Update firebase.ts

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -7,7 +7,7 @@ import { getFunctions } from "firebase/functions";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "##",
+    apiKey: "##", //this api key have been deprecated
     authDomain: "yt-clone-88b23.firebaseapp.com",
     projectId: "yt-clone-88b23",
     appId: "1:314722351841:web:dee3884dee2d43e15595a0",
